6b0e2d0f998023749e08d8bd0eb5fb535601f3c3
k-min9 <mingu4969@gmail.com>
2021-12-08 23:08:06 +0900

    add springboot with dependency

---
+++ pjt-final-spring/src/main/java/ssafy/moviecurators/MoviecuratorsApplication.java
@@ -0,0 +1,13 @@
+package ssafy.moviecurators;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviecuratorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviecuratorsApplication.class, args);
+	}
+
+}
